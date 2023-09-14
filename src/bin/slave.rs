e4b72bac7686416c9aa7404821f549425ddbb992
StormyXJ <1733599076@qq.com>
2023-09-14 15:25:59 +0800

    add slave set judge

--- src/bin/slave.rs
+++ src/bin/slave.rs
@@ -15,11 +15,18 @@ async fn main() {
     let slave_server:SocketAddr=slave_server.parse().unwrap();
     let slave_server=volo::net::Address::from(slave_server);
 
+    let master_addr = String::from(&inargs[2]);
+    let master_addr = format!("127.0.0.1:{}",master_addr);
+    let master_addr:SocketAddr=master_addr.parse().unwrap();
+    let master_addr=volo::net::Address::from(master_addr);
+
     let ss = SlaveServiceS{
         addr:slave_server.clone(),
+        master: master_addr.clone(),
     };
 
     volo_gen::miniredis::SlaveServiceServer::new(ss)
+        .layer_front(mini_redis::ContextLayer)
         .run(slave_server)
         .await
         .unwrap();
--- start_m_s.py
+++ start_m_s.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 # 读取修正后的JSON文件
 with open('master.config', 'r') as file:
     config_data = json.load(file)
@@ -7,16 +8,30 @@ with open('master.config', 'r') as file:
 # 处理JSON数据并生成所需的数据结构
 result_list = [(item['master'], item['slave']) for item in config_data]
 
-for master_port, slave_ports in result_list:
-    # 启动master任务
-    master_command = f'start cargo run --bin master {master_port} {" ".join(map(str, slave_ports))}'
-    os.system(master_command)
-    # print (master_command)
-    # subprocess.Popen(master_command, shell=True)
-    
-    # 启动slave任务
-    for slave_port in slave_ports:
-        slave_command = f'start cargo run --bin slave {slave_port}'
-        # subprocess.Popen(slave_command, shell=True)
-        os.system(slave_command)
+if 'linux' in sys.platform :
+    for master_port, slave_ports in result_list:
+        # 启动master任务
+        master_command = f'cargo run --bin master {master_port} {" ".join(map(str, slave_ports))} &'
+        os.system(master_command)
+        # print (master_command)
+        # subprocess.Popen(master_command, shell=True)
+        
+        # 启动slave任务
+        for slave_port in slave_ports:
+            slave_command = f'cargo run --bin slave {slave_port} &'
+            # subprocess.Popen(slave_command, shell=True)
+            os.system(slave_command)
+else :
+    for master_port, slave_ports in result_list:
+        # 启动master任务
+        master_command = f'start cargo run --bin master {master_port} {" ".join(map(str, slave_ports))}'
+        os.system(master_command)
+        # print (master_command)
+        # subprocess.Popen(master_command, shell=True)
+        
+        # 启动slave任务
+        for slave_port in slave_ports:
+            slave_command = f'start cargo run --bin slave {slave_port}'
+            # subprocess.Popen(slave_command, shell=True)
+            os.system(slave_command)
 
